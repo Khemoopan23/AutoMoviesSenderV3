9f4a8fbed83337e729607d502ae149ce4c8a7592
Vikash Kumar <116530215+anumitultrabots@users.noreply.github.com>
2022-11-03 15:31:23 +0530

    Update and rename info1.py to sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -2,8 +2,8 @@
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
 API_ID = 17737898
-API_HASH = 'ad762fe0516f367115ba651d929cf429'
-BOT_TOKEN = '5239800184:AAEqdtDX5LzMiLI5lto42dSbST0hu4J8M4c'
+API_HASH = ''
+BOT_TOKEN = ''
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -17,7 +17,7 @@ AUTH_USERS = []
 AUTH_CHANNEL = '-1001516360564'
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://amit:amit@cluster0.a1dlvvm.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = ""
 DATABASE_NAME = 'amit'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
